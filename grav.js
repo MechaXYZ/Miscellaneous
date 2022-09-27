2f18149d383cd2dda44036b6e0cc969332bb59e5
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2022-09-26 19:23:59 -0500

    Update grav.js

--- grav.js
+++ grav.js
@@ -7,7 +7,7 @@ if (answer != null) {
 
 var gravity = true;
 
-if (window.jQuery != undefined) {
+if (window.jQuery == undefined) {
 	var jq = document.createElement("script");
 	jq.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js";
 	document.body.appendChild(jq);
