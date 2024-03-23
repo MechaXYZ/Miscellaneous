3a3b6b96dce4c3cb9da7a3e3f1cb44ddd80fc76f
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2024-03-23 01:50:48 -0500

    Create AnimodulerTest7.lua

---
+++ AnimodulerTest7.lua
@@ -0,0 +1,258 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {
+	{
+		tm = 0;
+		["HumanoidRootPart"] = {
+			tm = .042;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .042;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .042;
+			cf = cfr(0,0,0,.342,-.94,0,.94,.342,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["KeyframeMarker"] = "hi";
+	};
+	{
+		tm = .042;
+		["HumanoidRootPart"] = {
+			tm = .041;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .041;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .041;
+			cf = cfr(0,0,0,-.087,-.936,-.341,.996,-.082,-.03,0,-.342,.94);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .083;
+		["HumanoidRootPart"] = {
+			tm = .041999999999999996;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .041999999999999996;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .041999999999999996;
+			cf = cfr(0,0,.001,-.601,-.478,-.64,.657,-.752,-.056,-.455,-.455,.766);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .125;
+		["HumanoidRootPart"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,0,.996,.087,0,-.087,.996,0,0,0,1);
+			es = Enum.PoseEasingStyle.Constant;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,.001,-.675,-.125,-.727,.517,-.784,-.345,-.527,-.608,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .167;
+		["HumanoidRootPart"] = {
+			tm = .04099999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04099999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04099999999999998;
+			cf = cfr(0,0,.001,-.68,-.095,-.727,.481,-.806,-.345,-.553,-.584,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .208;
+		["HumanoidRootPart"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04200000000000001;
+			cf = cfr(0,0,.001,-.683,-.071,-.727,.453,-.822,-.345,-.573,-.565,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .25;
+		["HumanoidRootPart"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,.001,-.685,-.047,-.727,.423,-.838,-.345,-.593,-.544,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .292;
+		["HumanoidRootPart"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,.001,-.686,-.036,-.727,.41,-.845,-.345,-.602,-.534,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .333;
+		["HumanoidRootPart"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,.001,-.686,-.036,-.727,.41,-.845,-.345,-.602,-.534,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .375;
+		["HumanoidRootPart"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .04199999999999998;
+			cf = cfr(0,0,.001,-.686,-.036,-.727,.41,-.845,-.345,-.602,-.534,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .417;
+		["HumanoidRootPart"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = .041000000000000036;
+			cf = cfr(0,0,.001,-.686,-.036,-.727,.41,-.845,-.345,-.602,-.534,.594);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+	{
+		tm = .458;
+		["HumanoidRootPart"] = {
+			tm = 0;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Torso"] = {
+			tm = 0;
+			cf = cfr(0,0,0,1,0,0,0,1,0,0,0,1);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+		["Right Arm"] = {
+			tm = 0;
+			cf = cfr(0,0,0,-.022,-.996,.09,1,-.023,-.01,.012,.09,.996);
+			es = Enum.PoseEasingStyle.Linear;
+			ed = "In";
+		};
+	};
+}
